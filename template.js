7268e43dec586ed49b33a1ec4e06edcb5eee7177
Antonio Villavicencio <jantoniovc@me.com>
2019-04-25 00:36:31 -0500

    Correct html template

--- template.js
+++ template.js
@@ -1,9 +1,9 @@
 export default ({markup, css}) => {
     return `<!doctype html>
-      <html lang="en">
+      <html lang="es">
         <head>
           <meta charset="utf-8">
-          <title>MERN Marketplace</title>
+          <title>mrKditos</title>
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
           <style>
